99d11ec065d154306e418c92e5b698639f216b78
Mate Elek <elekamate@gmail.com>
2018-07-04 12:00:02 +0200

    BankOfSimba project copied to the repo.

---
+++ BankOfSimba/BankOfSimba/Controllers/BankController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankOfSimba.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankOfSimba.Controllers
+{
+    public class BankController : Controller
+    {
+        public static List<BankAccount> bankAccounts = new List<BankAccount>()
+        {
+            new BankAccount("Simba", 2000, "Animal.Lion"),
+            new BankAccount("Bimba", 12000, "Animal.Lion"),
+            new BankAccount("Cimba", 3000, "Animal.Lion"),
+            new BankAccount("King", 3000, "Animal.Lion")
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Simba()
+        {
+            return View(bankAccounts);
+        }
+
+        [HttpPost]
+        public IActionResult IncreaseBalance(BankAccount bankaccount)
+        {
+
+            return RedirectToAction("Bank/Simba.cshtml");
+        }
+    }
+}
---
+++ BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public object AnimalType { get; set; }
+
+        public BankAccount(string name, int balance, object animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+    }
+}
